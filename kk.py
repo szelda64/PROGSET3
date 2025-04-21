9eaeddb597c4b4272b8b26005c2ba5dc4148858c
szelda64 <84426635+szelda64@users.noreply.github.com>
2025-04-20 20:16:34 -0400

    hiya

--- kk.py
+++ kk.py
@@ -1,13 +1,13 @@
 import heapq
-
+import numpy as np
 def kk_alg(a):
     print(f"Current sequence: {a}")
     a1 = []
     a2 = []
     maxes = heapq.nlargest(2,a)
-    max1 = maxes[0]
+    max1 = np.int64(maxes[0])
     print(f"Largest element: {max1}")
-    max2 = maxes[1]
+    max2 = np.int64(maxes[1])
     print(f"Second-largest element: {max2}")
     if(max2 == 0):
         print(f"Residue found: {max1}")
@@ -16,9 +16,10 @@ def kk_alg(a):
     a1.append(max1)
     a.remove(max2)
     a2.append(max2)
-    a.append(abs(max1 - max2))
-    a.append(0)
+    a.append(np.int64(abs(max1 - max2)))
+    a.append(np.int(0))
     return kk_alg(a)
 
 def rep_random(a):
+    sol = np.random.randint(low=1,high=len(a),size=len(a),dtype=np.int64)
     
