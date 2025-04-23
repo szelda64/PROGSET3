97a923ec56ff544870212dfb1bada186d61fd83d
KingMeMeMe <112827546+KingMeMeMe@users.noreply.github.com>
2025-04-22 20:50:26 -0400

    hill climbing plus i love you zekey

--- kk.py
+++ kk.py
@@ -1,7 +1,9 @@
 import heapq
 import numpy as np
+
 def kk_alg(a):
     print(f"Current sequence: {a}")
+    heapq.heapify(a)
     a1 = []
     a2 = []
     maxes = heapq.nlargest(2,a)
@@ -17,7 +19,7 @@ def kk_alg(a):
     a.remove(max2)
     a2.append(max2)
     a.append(np.int64(abs(max1 - max2)))
-    a.append(np.int(0))
+    a.append(int(0))
     return kk_alg(a)
 
 def sol_to_seq(a,b):
@@ -34,5 +36,20 @@ def rep_random(a):
         if(kk_alg(sol_to_seq(a,sol2)) < kk_alg(sol_to_seq(a,sol))):
             sol = sol2
     return sol
+
+def hill_climbing(a):
+    sol = np.random.randint(low=1,high=len(a),size=len(a),dtype=np.int64)
+
+    for _ in range(start=1,stop=25001):
+        sol2 = sol[:]
+        sol[np.random.randint(low=1,high=len(a),size=len(a),dtype=np.int64)] = -1 * sol[np.random.randint(low=1,high=len(a),size=len(a),dtype=np.int64)] 
+        if(kk_alg(sol_to_seq(a,sol2)) < kk_alg(sol_to_seq(a,sol))):
+            sol = sol2
+    return sol
+
+#def simul_anneal(a):
+
+
+example1 = [10,8,7,6,5]
 
 
